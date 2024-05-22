5a6d3eac2be91184d6fa1da5cfbb7def2daf6295
starwizard2005 <achyuth.hw@gmail.com>
2024-05-22 23:40:53 +0530

    Update solution_code.cpp

--- solution_code.cpp
+++ solution_code.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int mx = 0;
+int mx = -1;
 int f(int i, vector <int> &a, vector <int> &dp){
 
-    if(i > a.size()) return mx;
-    if (i == a.size()) return 0;
+    if(i >= a.size()) return mx;
+    if (i == a.size() - 1) return a[i];
     if(dp[i] != -1) return dp[i];
     int not_choose = a[i] + f(i+1, a, dp);
     int choose = a[i] + f(i+a[i]+1, a, dp);
@@ -21,8 +21,8 @@ int main(){
     for (int i = 0;i<n;i++)
         cin>>a[i];
     vector<int> dp(n, -1);
-    
-    for (int i = 1;i < n;i++){
+
+    for (int i = 0;i < n;i++){
         if (a[i] > mx)
             mx = a[i];
     }
